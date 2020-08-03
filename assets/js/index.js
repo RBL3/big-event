4f019a176d63f82bcc6f94256fdcce18255bd8f0
berlin <1449822833@qq.com>
2020-08-03 18:40:52 +0800

    后台页面内容区 切换功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,10 +15,37 @@ $(function () {
         }).then(res => {
             if (res.status === 0) {
                 console.log(res);
+                $(".userName span").text(res.data.username)
+                if (res.data.user_pic) {
+                    $(".layui-nav-img").prop("src", res.data.user_pic)
+                } else {
+                    $(".layui-nav-img").prop("src", "./assets/images/photo.JPG")
+                }
+
             } else {
                 location.href = `./login.html`
             }
 
         })
     })
+
+    // 内容主体区引入html
+
+    $(".option").click(function () {
+        $("iframe").prop("src", $(this).data("page"))
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
