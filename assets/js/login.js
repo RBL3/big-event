11e5ce49835354b2147baec77d58da77e26b30eb
berlin <1449822833@qq.com>
2020-08-03 11:35:08 +0800

    实现登录功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,9 +1,8 @@
 $(function () {
-    // 设置请求头
-    // /api/login
 
+    // 登录功能
     $("#loginForm").submit((e) => {
-        e.preventDefault()
+
         if ([...$(".layui-input")].every(el => !el.value)) {
             layui.layer.msg('用户名或密码为空');
             return
@@ -23,8 +22,34 @@ $(function () {
                 if (res.status === 0) {
                     localStorage.setItem('mytoken', res.token)
                     location.href = `./index.html?mytoken=${res.token}`
+                } else {
+                    layui.layer.msg('登录失败用户名或密码错误');
                 }
             })
-
     })
+
+
+
+    // 注册功能
+    $("#regForm").submit(function (e) {
+        e.preventDefault()
+    })
+
+
+    // 切换登录注册表单
+    let tablogin = false
+    $(".tab a ").click(function () {
+
+        if (tablogin) {
+            $(this).text("去注册账号")
+            $("#loginForm").css("display", "block")
+            $("#regForm").css("display", "none")
+        } else {
+            $(this).text("去登录")
+            $("#loginForm").css("display", "none")
+            $("#regForm").css("display", "block")
+        }
+        tablogin = !tablogin
+    })
+
 })
