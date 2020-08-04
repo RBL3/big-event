8a6111b16a4d0ac15b1499bdfd0cef90c1801871
berlin <1449822833@qq.com>
2020-08-05 00:18:37 +0800

    文章分类渲染 增加分类 删除分类部分

--- assets/js/category.js
+++ assets/js/category.js
@@ -20,8 +20,8 @@ $(function () {
                 // 获取渲染标签
                 var getTpl = tbody.innerHTML
 
-                // 获取根节点
-                view = document.querySelector(".form")
+                    // 获取根节点
+                    , view = document.querySelector(".form")
                 // 渲染
                 laytpl(getTpl).render(data, function (html) {
                     view.innerHTML = html;
@@ -31,13 +31,53 @@ $(function () {
     }
     render()
 
+    // 添加文章 /my/article/addcates  name  alias
+    $("#addBtn").on("click", function () {
+        layer.open({
+            title: "添加分类",
+            type: 1,
+            area: ['470px', '240px'], //宽高
+            content: addFrom.innerHTML
+        });
+        // console.log($("articleClass"));
+        $("#articleClass").submit(function (e) {
+            e.preventDefault()
+            $.ajax({
+                type: "post",
+                url: "/my/article/addcates",
+                data: $(this).serialize(),
+                headers: {
+                    Authorization: token
+                }
+            }).then(res => {
+                // console.log(res);
+                if (res.status === 0) {
+                    layer.closeAll();
+                    render()
+                } else {
+                    layui.layer.msg("添加失败", { time: 5000, icon: 5 });
+                }
+            })
+        })
+    })
+
+
     // 删除分类部分  del
     $(".form").on("click", function (e) {
-        console.log($(e.target).data("id"));
         let id = $(e.target).data("id")
         $.ajax({
             type: "get",
-            url: `/my/article/deletecate/:${id}`,
+            url: `/my/article/deletecate/${id}`,
+            headers: {
+                Authorization: token
+            }
+        }).then(res => {
+            if (res.status === 0) {
+                render()
+                layui.layer.msg("删除成功", { time: 2000, icon: 6 });
+            } else {
+                render()
+            }
         })
     })
 
